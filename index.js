ec5f2b00ce76dba771abf229612bc8ca32d5b3f0
Akif Manzoor <akifmanzoor124@gmail.com>
2020-09-17 13:29:41 -0400

    test

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.put('/api/persons/:id', (req, res, next) => {
         number: body.number
     }
   
-    db.findByIdAndUpdate(req.params.id, person, { new: true }, {runValidators: true})
+    db.findByIdAndUpdate(req.params.id, person, { new: true, runValidator: true })
       .then(updatedPerson => {
         res.json(updatedPerson)
       })
